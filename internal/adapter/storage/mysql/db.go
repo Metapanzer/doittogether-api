706f38e413d72274cba811773c26d6ce27f2e711
Krisna Sandy Pribadi <baltheon@gmail.com>
2025-01-01 14:05:34 +0700

    chore: setup DB connection

---
+++ internal/adapter/storage/mysql/db.go
@@ -0,0 +1,31 @@
+package mysql
+
+import (
+	"DoItTogether/internal/adapter/config"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	Conn *gorm.DB
+}
+
+func New(config *config.DB) (*DB, error) {
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Name,
+	)
+	dsn := connStr
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to database")
+	return &DB{Conn: db}, nil
+}
